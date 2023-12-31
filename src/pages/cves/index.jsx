a17a458ab3845971fe22cc84ac02fcef29828733
vijay22uk <vijay22uk@gmail.com>
2023-12-31 14:27:28 +0530

    ok

--- src/pages/cves/index.jsx
+++ src/pages/cves/index.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./index.css";
-
+// import doc from "./Runbook.docx"
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 const Index = () => {
-  return <p className="font-primary text-2xl">Coming soon</p>;
+  const docs = [
+    { uri: "https://runbook-store.surge.sh/docs/Runbooklink.docx" },
+  ];
+  return <div>
+    <DocViewer
+      config={{
+        header: {
+          disableHeader: true,
+          disableFileName: true,
+          retainURLParams: true,
+        },
+      }}
+      className="runbook-doc-viewer"
+      style={{ height: "90vh" }} documents={docs} pluginRenderers={DocViewerRenderers} onError={(err) => console.error('Document rendering error:', err)} />
+    {/* <FileViewer
+        fileType={"docx"}
+        filePath={"http://localhost:3000/docs/Runbook.docx"} /> */}
+
+
+  </div>
 };
 
 export default Index;
